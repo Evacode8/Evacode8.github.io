ba4afea75f3b969653d70f6f302b401031cb8db8
Evacode8 <129660461+Evacode8@users.noreply.github.com>
2023-05-07 11:46:55 +0200

    Update galerie.js

--- galerie.js
+++ galerie.js
@@ -82,6 +82,31 @@ anime.timeline({loop: true})
   });
 
 
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 40,
+
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor:"true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+ 
+});
+
   
 
 
